c11fd6ebb4a5a29571ae218cca5733a78859cad5
Vadim Kalinnikov <moose@ylsoftware.com>
2025-09-05 17:47:54 +0300

    GTK version ported from GTK2 to GTK3

--- pkmi-gtk3.pl
+++ pkmi-gtk3.pl
@@ -6,7 +6,7 @@ use diagnostics;
 
 use Data::Dumper;
 
-use Gtk2 '-init';
+use Gtk3 '-init';
 use Glib qw/TRUE FALSE/;
 
 # all info will be stored here
@@ -150,7 +150,7 @@ if (my $error = $@) {
 
 # Build UI
 
-my $modelMod = Gtk2::ListStore->new(
+my $modelMod = Gtk3::ListStore->new(
 		'Glib::String',
 		'Glib::String',
 		'Glib::String',
@@ -164,61 +164,61 @@ foreach my $modName (sort keys(%$moduleList)) {
 			2, $moduleList->{$modName}->{desc}
 		);
 }
-my $modelParam = Gtk2::ListStore->new(
+my $modelParam = Gtk3::ListStore->new(
 		'Glib::String',
 		'Glib::String',
 		'Glib::String',
 	);
 
-my $MainWindow = Gtk2::Window->new();
+my $MainWindow = Gtk3::Window->new();
 $MainWindow->set_title("Perl Kernel Module Info");
 
-my $paned = Gtk2::HPaned->new;
-my $listMod = Gtk2::TreeView->new($modelMod);
-my $listParam = Gtk2::TreeView->new($modelParam);
+my $paned = Gtk3::HPaned->new;
+my $listMod = Gtk3::TreeView->new($modelMod);
+my $listParam = Gtk3::TreeView->new($modelParam);
 
 foreach (['Module', 0], ['Type', 1], ['Description', 2]) {
-	$listMod->append_column(Gtk2::TreeViewColumn->new_with_attributes(
+	$listMod->append_column(Gtk3::TreeViewColumn->new_with_attributes(
 			$_->[0],
-			Gtk2::CellRendererText->new,
+			Gtk3::CellRendererText->new,
 			text => $_->[1]
 		));
 }
 
 foreach (['Param', 0], ['Value', 1], ['Description', 2]) {
-	$listParam->append_column(Gtk2::TreeViewColumn->new_with_attributes(
+	$listParam->append_column(Gtk3::TreeViewColumn->new_with_attributes(
 			$_->[0],
-			Gtk2::CellRendererText->new,
+			Gtk3::CellRendererText->new,
 			text => $_->[1]
 		));
 }
 
-my $scroll1 = Gtk2::ScrolledWindow->new;
+my $scroll1 = Gtk3::ScrolledWindow->new;
 $scroll1->set_size_request(360, -1);
 $scroll1->add($listMod);
 $paned->pack1($scroll1, 1, 0);
 
-my $scroll2 = Gtk2::ScrolledWindow->new;
+my $scroll2 = Gtk3::ScrolledWindow->new;
 $scroll2->set_size_request(360, -1);
 $scroll2->add($listParam);
 $paned->pack2($scroll2, 1, 0);
 
 
 # Create main menu
-my $mainMenu = Gtk2::MenuBar->new;
-my $menuFile = Gtk2::MenuItem->new('_File');
-my $subMenuFile = Gtk2::Menu->new;
-my $menuExit = Gtk2::MenuItem->new('Exit');
-$menuExit->signal_connect(activate => sub {Gtk2->main_quit});
+my $mainMenu = Gtk3::MenuBar->new;
+my $menuFile = Gtk3::MenuItem->new('_File');
+my $subMenuFile = Gtk3::Menu->new;
+my $menuExit = Gtk3::MenuItem->new('Exit');
+$menuExit->signal_connect(activate => sub {Gtk3->main_quit});
 $subMenuFile->append($menuExit);
 $menuFile->set_submenu($subMenuFile);
 $mainMenu->append($menuFile);
 
-my $menuHelp = Gtk2::MenuItem->new('_Help');
-my $subMenuHelp = Gtk2::Menu->new;
-my $menuAbout = Gtk2::MenuItem->new('About');
+my $menuHelp = Gtk3::MenuItem->new('_Help');
+my $subMenuHelp = Gtk3::Menu->new;
+my $menuAbout = Gtk3::MenuItem->new('About');
 $menuAbout->signal_connect(activate => sub {
-		my $dialog = Gtk2::MessageDialog->new(
+		my $dialog = Gtk3::MessageDialog->new(
 				$MainWindow,
 				'modal',
 				'info',
@@ -233,7 +233,7 @@ $menuHelp->set_submenu($subMenuHelp);
 $mainMenu->append($menuHelp);
 
 # Pack UI
-my $vbox = Gtk2::VBox->new;
+my $vbox = Gtk3::VBox->new;
 $vbox->pack_start($mainMenu, 0, 0, 0);
 $vbox->add($paned);
 
@@ -241,12 +241,12 @@ $vbox->add($paned);
 $MainWindow->add($vbox);
 $MainWindow->set_default_size(720, 400);
 
-$MainWindow->signal_connect(destroy => sub {Gtk2->main_quit});
+$MainWindow->signal_connect(destroy => sub {Gtk3->main_quit});
 $listMod->get_selection->signal_connect (changed => \&selectModule);
 
 $MainWindow->show_all;
 
-Gtk2->main;
+Gtk3->main;
 
 sub selectModule {
 	my $selection = $listMod->get_selection();
